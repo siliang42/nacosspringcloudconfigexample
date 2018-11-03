e6f277f24670e22720ab56d18303ec9d1b56374d
siliang42 <obanie@qq.com>
2018-11-03 19:11:07 +0800

    Initial commit

---
+++ src/main/java/com/jiayi/nacosspringcloudconfigexample/NacosSpringCloudConfig.java
@@ -0,0 +1,17 @@
+package com.jiayi.nacosspringcloudconfigexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @description :
+ * @author: CONCENTRATE
+ * @time: 2018/11/3.
+ */
+@SpringBootApplication
+public class NacosSpringCloudConfig {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosSpringCloudConfig.class, args);
+    }
+}
